708e7e6eb7576cf94b5285b00beeefc4dc84f8c0
Shlok Motwani <shlok.codes@gmail.com>
2025-01-23 16:45:34 +0530

    sign up form with validation

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ const session = require("express-session");
 const { Pool } = require("pg");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const PORT = process.env.PORT || 3000;
+const bcrypt = require("bcryptjs");
+const { validateUser } = require("./scripts/form-validator");
+const { validationResult } = require("express-validator");
+const PORT = process.env.PORT || 3001;
 
 const pool = new Pool({
-  host: "",
-  user: "",
-  password: "",
-  database: "",
-  port: "",
+  host: "localhost",
+  user: "a",
+  password: "a",
+  database: "clubhouse",
+  port: 5432,
 });
 
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.set(express.urlencoded({ extended: true }));
+app.set("views", [
+  path.join(__dirname, "views"),
+  path.join(__dirname, "partials"),
+]);
+app.use(express.urlencoded({ extended: true }));
 
 passport.use(new LocalStrategy(async (username, password, done) => {}));
 
@@ -28,7 +34,23 @@ passport.serializeUser((user, done) => {});
 passport.deserializeUser(async (id, done) => {});
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  //   res.render("index", { title: "Home" });
+  res.redirect("/sign-up");
+});
+
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up-form", { title: "Sign Up" });
+});
+
+app.post("/sign-up", validateUser, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("sign-up-form", {
+      title: "Sign Up",
+      errors: errors.array(),
+    });
+  }
+  return res.send("Signed Up!!");
 });
 
 app.listen(PORT, () => {
---
+++ scripts/form-validator.js
@@ -0,0 +1,47 @@
+const { body } = require("express-validator");
+
+const emptyFieldErr = "cannot be empty.";
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 2 and 50 characters.";
+const emailErr = "Please enter a valid email address.";
+const passwordFormatErr = `Your password must be a minimum of 6 characters long having lowercase letter, uppercase letter, numerical digit and special character (eg. !@#$%^).`;
+const passwordMismatchErr = "Passwords do not match.";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage(`First name ${emptyFieldErr}`)
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 2, max: 50 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .notEmpty()
+    .withMessage(`Last name ${emptyFieldErr}`)
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 2, max: 50 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage(`Email address ${emptyFieldErr}`)
+    .isEmail()
+    .withMessage(emailErr),
+  body("password")
+    .isStrongPassword({
+      minLength: 6,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+    .withMessage(passwordFormatErr),
+  body("confirmPassword")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage(passwordMismatchErr),
+];
+
+module.exports = { validateUser };
