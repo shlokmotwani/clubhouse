2f9ee8b3fecd4816099cabad50b4ecc56a6185f2
Shlok Motwani <shlok.codes@gmail.com>
2025-01-25 13:58:17 +0530

    fix: deployment credentials

--- config/pool.js
+++ config/pool.js
@@ -1,8 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString:
-    "postgresql://user:npg_DPcN5TZpbqK9@ep-noisy-mountain-a8da3bcj-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
+  connectionString:process.env.DB_CONNECTION_URL,
 });
 
 module.exports = { pool };
