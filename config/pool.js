dcaea95ee1b1f561e09fe4d4edf20df3d665e8a0
Shlok Motwani <shlok.codes@gmail.com>
2025-01-25 13:50:33 +0530

    replace with deployment credentials

--- config/pool.js
+++ config/pool.js
@@ -1,11 +1,8 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost",
-  user: "a",
-  password: "a",
-  database: "clubhouse",
-  port: 5432,
+  connectionString:
+    "postgresql://user:npg_DPcN5TZpbqK9@ep-noisy-mountain-a8da3bcj-pooler.eastus2.azure.neon.tech/neondb?sslmode=require",
 });
 
-module.exports = { pool }
+module.exports = { pool };
